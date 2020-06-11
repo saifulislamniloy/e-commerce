b2600fbe17acd55c896bac0365e39034267bfa35
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-06-11 12:08:40 +0600

    routing added

---
+++ src/router/AppRoute.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import { Route, Switch } from "react-router-dom";
+
+class AppRoute extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Switch>
+                </Switch>
+            </Fragment>
+        );
+    }
+}
+
+export default AppRoute;

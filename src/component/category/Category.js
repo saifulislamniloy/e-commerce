3b094c218b10b66f2809a41dd22ae3d253cfcfe2
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-06-11 12:15:42 +0600

    category component added

---
+++ src/component/category/Category.js
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from 'react'
+import { Container, Col, Card, Row } from 'react-bootstrap'
+import axios from 'axios';
+import AppUrl from '../../router/AppUrl';
+
+export default class Category extends Component {
+    constructor() {
+        super();
+        this.state = {
+            categoryListView: ""
+        }
+    }
+
+    componentDidMount() {
+        axios.get(AppUrl.categoryList)
+            .then(response => {
+                console.log(response);
+                this.setCatergories(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    setCatergories(data) {
+        const categoryData = data;
+        const view = categoryData.map(categoryData => {
+            return (<Col sm={12} md={6} lg={4} className="p-2">
+                <div>
+                    <Card className="card">
+                        <Card.Img variant="top" src={"" + categoryData.link} className="catergoryImage"/>
+                        <Card.Body>
+                            <Card.Title className="cardTitle">{categoryData.title}</Card.Title>
+                            <Card.Text className="subtitle">
+                                {categoryData.id}
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </Col>)
+        })
+        this.setState({ categoryListView: view })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Container>
+                    <Row>
+                        {this.state.categoryListView}
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+    }
+}
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from 'react'
 import TopNavigation from '../component/topContent/TopNavigation'
+import Category from '../component/category/Category'
 
 export default class HomePage extends Component {
     render() {
         return (
             <Fragment>
                 <TopNavigation />
+                <br/><br/><br/><br/>
+                <Category />
             </Fragment>
         )
     }

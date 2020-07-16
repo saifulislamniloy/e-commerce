93688cb91c3fee2384562664744ebb4e20779d47
saiful-islam-niloy <saifulislam65@iut-dhaka.edu>
2020-07-16 12:12:23 +0600

    cart item ui modified

--- src/component/cart/CartItems.js
+++ src/component/cart/CartItems.js
@@ -24,29 +24,29 @@ export default class CartItems extends Component {
             const view = arr.map(arr => {
                 return (<Col sm={12} md={12} lg={12} className="p-2">
                     <div>
-                        <Card>
+                        <Card className="p-4">
                             <Card.Body>
-                                <Row>
+                                <Row className="cart">
                                     <Col lg={2} md={2} sm={2}>
-                                        <img src={""+arr.img} alt="Girl in a jacket" width="80" height="80"/>
+                                        <img className="vertical-center" src={""+arr.img} alt="smiley" width="80" height="80"/>
                                     </Col>
                                     <Col lg={2} md={2} sm={2}>
-                                        <Card.Title className="title text-center">{arr.title}</Card.Title>
+                                        <Card.Title className="title text-center vertical-center">{arr.title}</Card.Title>
                                     </Col>
                                     <Col lg={2} md={2} sm={2}>
-                                        <Card.Title className="title text-center">{arr.price}</Card.Title>
+                                        <Card.Title className="title text-center vertical-center">{arr.price}</Card.Title>
+                                    </Col>
+                                    <Col lg={1} md={1} sm={1}>
+                                        <img className="vertical-center icon" onClick={() => this.add(arr.id)} src={plus} alt="delete"/>
+                                    </Col>
+                                    <Col lg={1} md={1} sm={1}>
+                                        <img className="vertical-center icon" onClick={() => this.remove(arr.id)} src={minus} alt="delete"/>
+                                    </Col>
+                                    <Col lg={1} md={1} sm={1}>
+                                        <img className="vertical-center icon" onClick={() => this.remove(arr.id)} src={deleteImg} alt="delete"/>
                                     </Col>
                                     <Col lg={3} md={3} sm={3}>
-                                        <Card.Title className="title text-center">{arr.quantity+"*5 = 5000 BDT"}</Card.Title>
-                                    </Col>
-                                    <Col lg={1} md={1} sm={1}>
-                                        <img onClick={() => this.add(arr.id)} src={plus} alt="delete" width="50" height="50"/>
-                                    </Col>
-                                    <Col lg={1} md={1} sm={1}>
-                                        <img onClick={() => this.remove(arr.id)} src={minus} alt="delete" width="50" height="50"/>
-                                    </Col>
-                                    <Col lg={1} md={1} sm={1}>
-                                        <img onClick={() => this.remove(arr.id)} src={deleteImg} alt="delete" width="50" height="50"/>
+                                        <Card.Title className="title text-center vertical-center">{arr.quantity+"*5 = 5000 BDT"}</Card.Title>
                                     </Col>
                                 </Row>
                             </Card.Body>
